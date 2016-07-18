b5050e1189c394ec8bef70487a876478c7bce3e4
Laury Bueno <laury@hacklab.com.br>
2016-07-18 15:06:49 -0300

    Script para importar fóruns leva em conta o destino de cada migração

--- paralapraca/management/commands/import_forum_moodle.py
+++ paralapraca/management/commands/import_forum_moodle.py
@@ -21,16 +21,6 @@ class Command(BaseCommand):
     args = 'file'
     help = 'import users'
 
-    # Returns the instance of the new forum wich must recieve
-    def new_forum(old_forum_id):
-        switcher = {
-            4: {
-                'forum' : '',
-                'category' : '',
-                'tag': ''
-            }
-        }
-
     @transaction.atomic
     def handle(self, *files, **options):
 
@@ -44,9 +34,9 @@ class Command(BaseCommand):
         me = User.objects.all().first()
 
         # Create all categories
-        catPauta, _ = Category.objects.get_or_create(title='Pautas',description='Pautas',color='FFF')
-        catReg, _ = Category.objects.get_or_create(title='Registros',description='Registros',color='FFF')
-        catDisc, _ = Category.objects.get_or_create(title='Discussões',description='Discussões',color='FFF')
+        catPauta, _ = Category.objects.get_or_create(name='Pautas',description='Pautas',color='FFF')
+        catReg, _ = Category.objects.get_or_create(name='Registros',description='Registros',color='FFF')
+        catDisc, _ = Category.objects.get_or_create(name='Discussões',description='Discussões',color='FFF')
 
         # Create all foruns
         fGE, _ = Forum.objects.get_or_create(author=me, title='Fórum Geral')
@@ -58,6 +48,105 @@ class Command(BaseCommand):
         fOG, _ = Forum.objects.get_or_create(author=me, title='Fórum Olinda - Gestão Paralapraca')
         fGM, _ = Forum.objects.get_or_create(author=me, title='Fórum Gestão Municipal')
 
+        # Create the nedded tags for the migrations
+        tOrg, _ = Tag.objects.get_or_create(name='Organização de Ambiente')
+        tArt, _ = Tag.objects.get_or_create(name='Artes Visuais')
+        tPai, _ = Tag.objects.get_or_create(name='Paisagens Culturais')
+        tBri, _ = Tag.objects.get_or_create(name='Brincar')
+        tExp, _ = Tag.objects.get_or_create(name='Exploração de Mundo')
+        tLit, _ = Tag.objects.get_or_create(name='Literatura')
+        tMus, _ = Tag.objects.get_or_create(name='Música')
+
+        # Holds all the necesseary exchange info for each source and destination forum in the migration
+        exchange = {
+            4: {
+                'forum' : fGE,
+                'category' : catDisc,
+            },
+            15: {
+                'forum' : fAS,
+            },
+            27: {
+                'forum' : fGE,
+                'category' : catReg,
+                'tag' : tOrg,
+            },
+            22: {
+                'forum' : fGE,
+                'category' : catReg,
+                'tag' : tArt,
+            },
+            7: {
+                'forum' : fGE,
+                'category' : catPauta,
+            },
+            5: {
+                'forum' : fGE,
+                'category' : catReg,
+                'tag' : tPai,
+            },
+            23: {
+                'forum' : fGE,
+                'category' : catReg,
+                'tag' : tBri,
+            },
+            25: {
+                'forum' : fGE,
+                'category' : catReg,
+                'tag' : tExp,
+            },
+            6: {
+                'forum' : fGE,
+                'category' : catReg,
+            },
+            18: {
+                'forum' : fGE,
+                'category' : catPauta,
+                'tag' : tLit,
+            },
+            26: {
+                'forum' : fGE,
+                'category' : catReg,
+                'tag' : tMus,
+            },
+            20: {
+                'forum' : fGE,
+                'category' : catPauta,
+                'tag' : tMus,
+            },
+            8: {
+                'forum' : fGE,
+                'category' : catPauta,
+                'category2' : catReg,
+            },
+            16: {
+                'forum' : fGE,
+                'category' : catPauta,
+                'tag' : tArt,
+            },
+            52: {
+                'forum' : fGE,
+            },
+            21: {
+                'forum' : fCG,
+            },
+            47: {
+                'forum' : fGM,
+            },
+            51: {
+                'forum' : fGE,
+                'category' : catPauta,
+                'tag' : tBri,
+            },
+            17: {
+                'forum' : fGE,
+                'category' : catPauta,
+                'tag' : tExp,
+            },
+            43: {
+                'forum' : fMG,
+            },
+        }
 
         # The following variable will store every discussion in a dictionary
         topics = {}
@@ -71,9 +160,24 @@ class Command(BaseCommand):
                         row[fieldname] = row[fieldname][:size]
 
 
+                # Find out wich forum is the corret one for the current topic
+                destination = exchange.get(int(row['forum']))
 
-                topics[row['id']] = Topic.objects.create(forum=fGE, title=row['name'], author=me)
-                topics[row['id']].categories.add(catPauta)
+
+                # TODO find out who is the real author of the current topic
+                # TODO repost with original created date
+                # TODO flag post as read-only
+                topics[row['id']] = Topic.objects.create(forum=destination['forum'], title=row['name'], author=me)
+
+                # Add all categories relevant to this topic1
+                if 'category' in destination:
+                    topics[row['id']].categories.add(destination['category'])
+                    if 'category2' in destination:
+                        topics[row['id']].categories.add(destination['category2'])
+
+                # Add all tags relevant to this topic
+                if 'tag' in destination:
+                    topics[row['id']].tags.add(destination['tag'])
 
                 count += 1
                 if count % 100 == 0:
@@ -112,7 +216,8 @@ class Command(BaseCommand):
                             # If it is, the parent can be safely used
                             parent_comment = comments[row['parent']]
 
-                        # import pdb; pdb.set_trace()
+
+                        # TODO adjust user in each comment
                         comments[row['id']] = Comment.objects.create(
                             topic=topics[row['discussion']],
                             text=row['message'],
@@ -120,13 +225,12 @@ class Command(BaseCommand):
                             parent=parent_comment
                         )
                     else:
+                        # TODO adjust user in each comment
                         comments[row['id']] = Comment.objects.create(
                             topic=topics[row['discussion']],
                             text=row['message'],
                             author=me
                         )
-
-                    # TODO adjust user in each comment
 
                     count += 1
                     if count % 100 == 0:
