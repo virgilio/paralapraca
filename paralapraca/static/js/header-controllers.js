e0842222b92e16dfc2fbf68d7e6b4842851363f5
buruno <nddled@gmail.com>
2016-07-27 18:13:20 -0300

    chamadas para as notificações na dashboard

--- paralapraca/static/js/header-controllers.js
+++ paralapraca/static/js/header-controllers.js
@@ -2,10 +2,35 @@
     'use strict';
     var app = angular.module('header.controllers', []);
 
-    app.controller('HeaderCtrl', ['$scope',
-        function ($scope) {
-            $scope.show_notification = false;
+    app.controller('HeaderCtrl', ['$scope','Notification',
+        function ($scope, Notification) {
+            $scope.show_notification = true;
+            $scope.notifications_loaded = false;
+            $scope.notifications = Notification.query(function(res){
+                $scope.notifications_loaded = true;
+            });
         }
     ]);
+
+    app.filter('actionFilter',function(){
+        return function(txt) { // [ação no] tópico tal
+            var filtered;
+            switch(txt) {
+                case 'new_comment':
+                    filtered = 'comentou no';
+                    break;
+                case 'new_topic':
+                    filtered = 'criou o';
+                    break;
+                case 'new_reaction':
+                    filtered = 'gostou do';
+                    break;
+                case 'new_reaction_comment':
+                    filtered = 'gostou do comentário no';
+                    break;
+            }
+            return filtered;
+        }
+    });
 
 })(window.angular);
