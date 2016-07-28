079da8622d28c2ba08110f858d2b6b0e2db34158
buruno <nddled@gmail.com>
2016-07-28 16:01:40 -0300

    filtro para intervalo de datas nas notificações

--- paralapraca/static/js/header-controllers.js
+++ paralapraca/static/js/header-controllers.js
@@ -4,7 +4,7 @@
 
     app.controller('HeaderCtrl', ['$scope','Notification',
         function ($scope, Notification) {
-            $scope.show_notification = false;
+            $scope.show_notification = true;
             $scope.notifications_loaded = false;
             $scope.notifications = Notification.query(function(res){
                 $scope.notifications_loaded = true;
@@ -30,6 +30,43 @@
                     break;
             }
             return filtered;
+        }
+    });
+
+    app.filter('dateFilter',function(){
+        return function(dt) {
+            /*var test = "2016-12-2"+Math.floor(Math.random() * 10)+"T14:58:48.179227Z",
+                max = "2016-12-31T20:58:42.346288Z";
+            console.log(max+" - "+test)*/
+            var past = new Date(dt),
+                now = new Date(),
+                diff = now.getTime() - past.getTime(),
+                labels = {
+                    'ano': 31536000000,
+                    'mês': 2592000000,
+                    'semana': 604800000,
+                    'dia': 86400000,
+                    'hora': 3600000,
+                    'minuto': 6000,
+                    'segundo': 1000
+                },
+                time_int,
+                filtered = [];
+            angular.forEach(labels,function(val,time_unit){
+                time_int = Math.floor(diff/val);
+                if(time_int*1000<=val && time_int > 0) {
+                    if(time_int > 1) {
+                        if(time_unit == "mês") {
+                            time_unit = 'meses';
+                        }
+                        else {
+                            time_unit = time_unit+'s';
+                        }
+                    }
+                    filtered.push(time_int+" "+time_unit);
+                }
+            });
+            return filtered[0];
         }
     });
 
