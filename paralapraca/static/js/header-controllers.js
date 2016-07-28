7b27def2a1b34042db3f7539d1ae25fa62c6c6bb
buruno <nddled@gmail.com>
2016-07-28 18:20:48 -0300

    refinando o filtro das notificações e pequenas correções

--- paralapraca/static/js/header-controllers.js
+++ paralapraca/static/js/header-controllers.js
@@ -4,7 +4,7 @@
 
     app.controller('HeaderCtrl', ['$scope','Notification',
         function ($scope, Notification) {
-            $scope.show_notification = true;
+            $scope.show_notification = false;
             $scope.notifications_loaded = false;
             $scope.notifications = Notification.query(function(res){
                 $scope.notifications_loaded = true;
@@ -26,7 +26,7 @@
                     filtered = 'gostou do';
                     break;
                 case 'new_reaction_comment':
-                    filtered = 'gostou do comentário no';
+                    filtered = 'gostou de um comentário no';
                     break;
             }
             return filtered;
@@ -35,9 +35,6 @@
 
     app.filter('dateFilter',function(){
         return function(dt) {
-            /*var test = "2016-12-2"+Math.floor(Math.random() * 10)+"T14:58:48.179227Z",
-                max = "2016-12-31T20:58:42.346288Z";
-            console.log(max+" - "+test)*/
             var past = new Date(dt),
                 now = new Date(),
                 diff = now.getTime() - past.getTime(),
@@ -47,14 +44,16 @@
                     'semana': 604800000,
                     'dia': 86400000,
                     'hora': 3600000,
-                    'minuto': 6000,
+                    'minuto': 60000,
                     'segundo': 1000
                 },
                 time_int,
                 filtered = [];
             angular.forEach(labels,function(val,time_unit){
                 time_int = Math.floor(diff/val);
-                if(time_int*1000<=val && time_int > 0) {
+                if(diff>=val && time_int > 0) {
+                                    console.log(time_int+" "+time_unit)
+
                     if(time_int > 1) {
                         if(time_unit == "mês") {
                             time_unit = 'meses';
