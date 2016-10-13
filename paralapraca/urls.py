7905b31055860b37745d61a98bf10a436cb9d5b9
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2016-10-13 15:06:56 -0300

    Revert "provide oauth views here (not a good idea) refs #181"
    This reverts commit da437a4ee07fea4654632d85524cbf98f05ebe31.

--- paralapraca/urls.py
+++ paralapraca/urls.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import patterns, include, url
+from django.conf.urls import url
 from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name="base.html")),
-    url(r'^o2/me/?', views.OAuth2UserInfoView.as_view(), name='oauth2_provider_userinfo'),
-]
+    url(r'', TemplateView.as_view(template_name="base.html")),
+    ]
