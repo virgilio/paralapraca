bc856a1751fd05704b6545c7c1d70373df66f9fb
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2016-10-20 17:52:19 -0200

    add screen and auth to show rocketchat inside timtec

--- paralapraca/urls.py
+++ paralapraca/urls.py
@@ -5,5 +5,7 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    url(r'', TemplateView.as_view(template_name="base.html")),
-    ]
+    url(r'^$', TemplateView.as_view(template_name="base.html")),
+    url(r'^chat/?$', views.ChatScreenView.as_view(template_name="chat.html"), name='chat_screen'),
+    url(r'^_chat/auth/$', views.RocketchatIframeAuthView.as_view(), name='rocketchat_iframe_auth'),
+]
---
+++ paralapraca/views.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+import json
+
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, HttpResponseForbidden
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic.base import TemplateView, View
+
+
+ROCKET_CHAT = {
+    'address': 'http://chat.paralapraca.org.br',
+    'service': 'paralapraca'
+}
+
+
+class ChatScreenView(TemplateView):
+    template_name = 'chat.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ChatScreenView, self).get_context_data(**kwargs)
+        context['rocketchat'] = getattr(settings, 'ROCKET_CHAT', ROCKET_CHAT)
+        return context
+
+
+class RocketchatIframeAuthView(TemplateView):
+    template_name = 'rocketchat.html'
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(RocketchatIframeAuthView, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(RocketchatIframeAuthView, self).get_context_data(**kwargs)
+        context['rocketchat'] = getattr(settings, 'ROCKET_CHAT', ROCKET_CHAT)
+        return context
+
+    def get(self, request, *argz, **kwargz):
+        response = super(RocketchatIframeAuthView, self).get(request, *argz, **kwargz)
+        return response
+
+    def post(self, request, *argz, **kwargz):
+        origin = request.META.get('HTTP_ORIGIN', '')
+        token = request.COOKIES.get('rc_token', '')
+
+        response = HttpResponse(json.dumps({
+            'token': token
+        }))
+
+        response['Content-Type'] = 'application/json'
+        response['Access-Control-Allow-Origin'] = origin
+        response['Access-Control-Allow-Credentials'] = 'true'
+        response['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE'
+        response["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept"
+
+        return response
