1237a957cd30f487e0e06dd60c0727c7d5d3eebb
Laury Bueno <laury@hacklab.com.br>
2016-10-20 17:58:19 -0200

    Tela de carregamento agora está aplicada em todas as áreas do site

--- paralapraca/static/js/header-app.js
+++ paralapraca/static/js/header-app.js
@@ -8,5 +8,12 @@
         'discussion.services',
         'djangular',
         'gettext',
+        'ngAnimate',
+        'angular-loading-bar',
     ]);
+
+    app.config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
+        cfpLoadingBarProvider.spinnerTemplate = '<div class="" style="top: 0; bottom: 0; left: 0; right: 0; position: fixed; background: rgba(255,255,255,0.6);"></div><div class="fa fa-spinner"></div>';
+    }]);
+
 })(angular);
