93d907c8ae30502d4507b98e5131ee391293d2eb
Laury Bueno <laury@hacklab.com.br>
2016-11-04 18:55:46 -0200

    AnswerNotification criada (refs #283)

---
+++ paralapraca/migrations/0001_initial.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('activities', '0004_auto_20160817_1837'),
+        ('discussion', '0015_auto_20161104_1111'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='AnswerNotification',
+            fields=[
+                ('topicnotification_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='discussion.TopicNotification')),
+                ('activity', models.ForeignKey(to='activities.Activity')),
+            ],
+            bases=('discussion.topicnotification',),
+        ),
+    ]
--- paralapraca/models.py
+++ paralapraca/models.py
@@ -1,2 +1,9 @@
 # -*- coding: utf-8 -*-
+from django.db import models
+from discussion.models import TopicNotification
+from activities.models import Activity
 
+
+class AnswerNotification(TopicNotification):
+
+    activity = models.ForeignKey(Activity)
