4403a24709b1b56b9810d35f3c4d145833af061f
Laury Bueno <laury@hacklab.com.br>
2016-12-20 18:59:45 -0200

    Paralapracá agora tem sua própria configuração TinyMCE e aceita uploads de imagens (refs #324)

---
+++ paralapraca/static/js/ui.tinymce.paralapraca.config.js
@@ -0,0 +1,82 @@
+(function(angular){
+    'use strict';
+
+    var app = angular.module('ui.tinymce');
+
+    // If you want to cutomize uiTinymceConfig for your theme, please copy this file to you theme in the
+    // same path (static/js/). This are the default setting for timtec
+    app.value('uiTinymceConfig', {
+        base_url: '/static/tinymce-dist/',
+        related_url: true,
+        inline: false,
+        menubar: false,
+        relative_urls: false,
+        plugins : 'advlist lists autolink link image media autoresize',
+        toolbar: 'bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | quicklink link image media fullscreen',
+        skin: 'lightgray',
+        theme : 'modern',
+        language: 'pt_BR',
+        language_url : '/static/vendor/tinymce/langs/pt_BR.js',
+        resize: true,
+        elementpath: false,
+
+        // media customizations
+        media_poster: false,
+        media_alt_source: false,
+        media_dimensions: false,
+        media_url_resolver: function (data, resolve/*, reject*/) {
+            var complete_url = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+            var result = complete_url.exec(data.url);
+            if (result && result[2].length == 11) {
+                var youtube_id = result[2];
+                var embedHtml = '<iframe width="560" height="315" src="https://www.youtube.com/embed/'+ youtube_id +'" frameborder="0" allowfullscreen></iframe>';
+                resolve({html: embedHtml});
+            } else {
+                resolve({html: ''});
+            }
+        },
+
+        // Image upload
+        // enable title field in the Image dialog
+        image_title: false,
+        image_class_list: [
+            {title: 'Imagem', value: 'tinymce-image'}
+        ],
+        image_description: false,
+        image_dimensions: false,
+        // enable automatic uploads of images represented by blob or data URIs
+        automatic_uploads: true,
+        // here we add custom filepicker only to Image dialog
+        file_picker_types: 'image',
+        // and here's our custom image picker
+        file_picker_callback: function(cb, value, meta) {
+            var input = document.createElement('input');
+            input.setAttribute('type', 'file');
+            input.setAttribute('accept', 'image/*');
+
+            // Note: In modern browsers input[type="file"] is functional without
+            // even adding it to the DOM, but that might not be the case in some older
+            // or quirky browsers like IE, so you might want to add it to the DOM
+            // just in case, and visually hide it. And do not forget do remove it
+            // once you do not need it anymore.
+
+            input.onchange = function() {
+                var file = this.files[0];
+
+                // Note: Now we need to register the blob in TinyMCEs image blob
+                // registry. In the next release this part hopefully won't be
+                // necessary, as we are looking to handle it internally.
+                var id = 'blobid' + (new Date()).getTime();
+                var blobCache = tinymce.activeEditor.editorUpload.blobCache;
+                var blobInfo = blobCache.create(id, file);
+                blobCache.add(blobInfo);
+
+                // call the callback and populate the Title field with the file name
+                cb(blobInfo.blobUri(), { title: file.name });
+            };
+
+            input.click();
+          }
+    });
+
+})(window.angular);
