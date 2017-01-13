3a74502dec74e296b1de39a5fc8858471f607e86
Laury Bueno <laury@hacklab.com.br>
2017-01-13 19:22:44 -0200

    Notificações de chat são limitadas a um máximo de 9 (refs #323)

--- paralapraca/static/js/main-nav/controller.js
+++ paralapraca/static/js/main-nav/controller.js
@@ -6,12 +6,24 @@
         '$scope',
         function ($scope) {
             $scope.chat = {};
+
+            // Listens to 'message' events triggered by the Rocket Chat iframe
             window.addEventListener('message', function(e) {
+                var counter;
                 if(e.data.eventName === 'unread-changed'){
-                    if(e.data.data !== "")
-                        $scope.chat.notifications = e.data.data;
-                    else
+                    counter = e.data.data;
+                    if(counter > 9)
+                        // if there are more than 9 notifications, reduce the number
+                        $scope.chat.notifications = "9+";
+                    else if(e.data.data === "")
+                        // If the data value is empty, then it must be set to zero
                         $scope.chat.notifications = 0;
+                    else
+                        // Otherwise, the number must be used as is
+                        $scope.chat.notifications = e.data.data;
+
+                    // Ensures that the template is updated as soon as possible
+                    $scope.$apply();
                 }
             });
         }
