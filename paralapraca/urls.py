905291d764285dbef92b62b773e6812ab7ebbe36
Tiago Fassoni <tiago@cappuccino.works>
2017-06-30 19:47:10 -0300

    Serializers novos

--- paralapraca/urls.py
+++ paralapraca/urls.py
@@ -2,12 +2,16 @@
 from django.conf.urls import url, include
 from django.views.generic import TemplateView
 from rest_framework import routers
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
 
 router = routers.SimpleRouter(trailing_slash=False)
 router.register(r'answer-notification', views.AnswerNotificationViewSet)
 router.register(r'unread-notification', views.UnreadNotificationViewSet)
+router.register(r'summary', views.SummaryViewSet, base_name='summary')
+router.register(r'users-by-group', views.UsersByGroupViewSet, base_name='users-by-group')
+router.register(r'users-by-class', views.UsersByClassViewSet, base_name='users-by-class')
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="base.html")),
