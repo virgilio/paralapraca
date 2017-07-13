0baa01b0246bd6893449057c33266bef0a33ad47
Laury Bueno <laury@hacklab.com.br>
2017-07-13 15:35:08 -0300

    Old development environment files removed

--- docker-compose-update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /usr/src/timtec
-npm install
-bower --allow-root install
-pip install --no-cache-dir ipdb
-python /usr/src/timtec/manage.py migrate
-python /usr/src/timtec/manage.py runserver 0.0.0.0:8000
