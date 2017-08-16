c196b9db0b8eb07876004054e033b812f6176e62
Laury Bueno <laury@hacklab.com.br>
2017-08-16 19:52:37 -0300

    [EAD] Students group is now artificially added for Timtec compatibility

--- paralapraca/forms.py
+++ paralapraca/forms.py
@@ -1,8 +1,11 @@
 from accounts.forms import AcceptTermsForm
+from django.contrib.auth.models import Group
 
 
 class SignupForm(AcceptTermsForm):
 
     def signup(self, request, user):
+        # Add the new user to the students group for Timtec compatibility
+        user.groups.add(Group.objects.get(name='students'))
         user.accepted_terms = self.cleaned_data['accept_terms']
         user.save()
