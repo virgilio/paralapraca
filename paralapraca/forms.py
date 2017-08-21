52f298b3fdf360a26cfb21edb450d68715a05f9a
Laury Bueno <laury@hacklab.com.br>
2017-08-21 17:10:35 -0300

    First and last names added to the signup form

--- paralapraca/forms.py
+++ paralapraca/forms.py
@@ -1,11 +1,19 @@
 from accounts.forms import AcceptTermsForm
 from django.contrib.auth.models import Group
+from django import forms
 
 
 class SignupForm(AcceptTermsForm):
 
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+
     def signup(self, request, user):
         # Add the new user to the students group for Timtec compatibility
         user.groups.add(Group.objects.get(name='students'))
+
+        user.first_name = self.cleaned_data.get('first_name')
+        user.last_name = self.cleaned_data.get('last_name')
+
         user.accepted_terms = self.cleaned_data['accept_terms']
         user.save()
