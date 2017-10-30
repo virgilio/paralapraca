57d82e0f617ac71e15b2a58db60a541885480c31
Leonardo Piccioni de Almeida <leopiccionia@gmail.com>
2017-10-30 14:41:54 -0200

    'Hello, Angular' from contracts.

--- paralapraca/static/js/contracts/controllers.js
+++ paralapraca/static/js/contracts/controllers.js
@@ -8,21 +8,24 @@
         }
     ]);
 
-    app.controller('ContractDetailsCtrl', ['$scope', 'Contracts',
-        function ($scope, Contracts) {
-
+    app.controller('ContractDetailsCtrl', ['$scope', '$routeParams', 'Contracts',
+        function ($scope, $routeParams, Contracts) {
+            $scope.contract_id = $routeParams.contractId;
+            $scope.contract = Contracts.get({id: $scope.contract_id});
         }
     ]);
 
     app.controller('NewContractCtrl', ['$scope', 'Contracts',
         function ($scope, Contracts) {
-
+            $scope.editing_mode = false;
         }
     ]);
 
-    app.controller('EditContractCtrl', ['$scope', 'Contracts',
-        function ($scope, Contracts) {
-
+    app.controller('EditContractCtrl', ['$scope', '$routeParams', 'Contracts',
+        function ($scope, $routeParams, Contracts) {
+            $scope.editing_mode = true;
+            $scope.contract_id = $routeParams.contractId;
+            $scope.contract = Contracts.get({id: $scope.contract_id});
         }
     ]);
 
