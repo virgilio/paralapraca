2cff36f0ff57f881b8b63018138ab779185f006e
Leonardo Piccioni de Almeida <leopiccionia@gmail.com>
2017-11-06 14:58:35 -0200

    Integrating Course API with contracts.

--- paralapraca/static/js/contracts/controllers.js
+++ paralapraca/static/js/contracts/controllers.js
@@ -15,8 +15,10 @@
         }
     ]);
 
-    app.controller('NewContractCtrl', ['$scope', 'Contracts',
-        function ($scope, Contracts) {
+    app.controller('NewContractCtrl', ['$scope', 'Contracts', 'Courses',
+        function ($scope, Contracts, Courses) {
+            $scope.courses = Courses.query();
+
             $scope.editing_mode = false;
             $scope.contract = {
                 name: ''
@@ -24,8 +26,10 @@
         }
     ]);
 
-    app.controller('EditContractCtrl', ['$scope', '$routeParams', 'Contracts',
-        function ($scope, $routeParams, Contracts) {
+    app.controller('EditContractCtrl', ['$scope', '$routeParams', 'Contracts', 'Courses',
+        function ($scope, $routeParams, Contracts, Courses) {
+            $scope.courses = Courses.query();
+
             $scope.editing_mode = true;
             $scope.contract_id = $routeParams.contractId;
             $scope.contract = Contracts.get({id: $scope.contract_id});
